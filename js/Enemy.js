ab39080b4f4f49b49932df77b36a851fa2ab677b
Guillermo García López <memowii@hotmail.com>
2017-03-01 18:12:43 -0600

    Delete commentaries, and create the getSpeed and getPosition functions

--- js/Enemy.js
+++ js/Enemy.js
@@ -1,26 +1,28 @@
-// Enemies our player must avoid
 var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-    this.x = 12;
-    this.y = 10;
-    this.speed = 350;
+    this.x = -125;
+    this.y = this.getPosition();
+    this.speed = this.getSpeed();
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
+Enemy.prototype.getPosition = function () {
+    var enemyPositions = [58, 143, 227],
+        randomPos = Math.floor(Math.random() * 3);
+
+    return enemyPositions[randomPos];
+};
+
+Enemy.prototype.getSpeed = function () {
+    min = Math.ceil(200);
+    max = Math.floor(450);
+
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
     this.x += this.speed * dt;
 };
 
-// Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
