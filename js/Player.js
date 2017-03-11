4e810d81016235cbe3479a7b531f90006b700791
Guillermo García López <memowii@hotmail.com>
2017-03-10 18:51:43 -0600

    Make player move

--- js/Player.js
+++ js/Player.js
@@ -15,6 +15,27 @@ Player.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-Player.prototype.handleInput(keycode) {
+Player.prototype.handleInput = function (keycode) {
 
+    if (keycode === 'left' && !(this.x - 100 < 0)) {
+        console.log('entro left 1');
+        this.x += -100;
+    }
+
+    else if (keycode === 'right' && this.x + 100 < 505 - 101) {
+        this.x += 100;
+    }
+
+    else  if (keycode === 'up') {
+        if (!(this.y - 80 < 0)) {
+            this.y += -80;
+        } else {
+            this.x = 200;
+            this.y = 380;
+        }
+    }
+
+    else if (keycode === 'down' && this.y + 80 < 606 - 171) {
+            this.y += 80;
+    }
 };
